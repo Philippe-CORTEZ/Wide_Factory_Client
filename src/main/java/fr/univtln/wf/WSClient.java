b20ffe583a35138c59fe34dad5ae4f5cd1a710f7
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-03 13:35:23 +0200

    Update WS client

--- src/main/java/fr/univtln/wf/WSClient.java
+++ src/main/java/fr/univtln/wf/WSClient.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 
 /**
  * The WebSocket client using Tyrus
@@ -16,6 +17,25 @@ import java.net.URI;
 public class WSClient
 {
     /**
+     * Methode called when the client open a websocket connection with a server
+     * @param session describe the websocket connection
+     */
+    @OnOpen
+    public void onOpen(Session session)
+    {
+        try
+        {
+            // For the moment send just a message to communicate with the server
+            session.getBasicRemote().sendText("start");
+        }
+        catch (IOException error)
+        {
+            log.error("Error in onOpen : ", error);
+        }
+    }
+
+
+    /**
      * Methode called when the client receive a websocket message
      * @param message the message received
      * @param session describe the websocket connection
@@ -24,6 +44,15 @@ public class WSClient
     public void onMessage(String message, Session session)
     {
         log.info("msg " + message);
+
+        // Transform the string message (JSON formated) into Skeleton list
+        List<Skeleton> skeletons = Skeleton.newInstance(message);
+
+        // Display the movement
+        Main t = new Main();
+        Main.movement.setName("Test");
+        Main.movement.setSkeletons(skeletons);
+        t.start();
     }
 
 
@@ -39,7 +68,6 @@ public class WSClient
         try
         {
             Session session = container.connectToServer(WSClient.class, URI.create("ws://localhost:9001/monTest"));
-            session.getBasicRemote().sendText("Message from Tyrus");
         }
         catch (DeploymentException | IOException ex)
         {
