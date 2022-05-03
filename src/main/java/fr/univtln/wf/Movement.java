ca715d50e313bbd3ad0ae8546a1c22315d171949
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-03 11:13:03 +0200

    Add consturctor into Movement class

--- src/main/java/fr/univtln/wf/Movement.java
+++ src/main/java/fr/univtln/wf/Movement.java
@@ -8,17 +8,21 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * class that represent a movement
+ * @author Wide Factory Team
+ */
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Getter
 @Setter
-/**
- * class that represent a movement
- */
-public class Movement {
+public class Movement
+{
+    /** The movement name */
     private String name;
+    /** List of skeletons that represent the movement */
     private List<Skeleton> skeletons;
 
     /**
@@ -26,11 +30,23 @@ public class Movement {
      * convert un json file to an object movement
      * @param nameFileJson name of the json file
      * @param nameMovement name of the movement
-     * @throws IOException
+     * @throws IOException during the opening of the JSON file
      */
     public Movement(String nameFileJson, String nameMovement) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         skeletons = objectMapper.readValue(new File(nameFileJson), new TypeReference<List<Skeleton>>(){});
         name = nameMovement;
     }
+
+    /**
+     * Constructor
+     * @param skeletons list of Skeletons
+     * @param name the movement name
+     */
+    public Movement(List<Skeleton> skeletons, String name)
+    {
+        this.name = name;
+        this.skeletons = skeletons;
+    }
+
 }
