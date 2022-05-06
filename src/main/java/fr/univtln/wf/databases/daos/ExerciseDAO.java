9aef2933db76a0fe9555e1cf925598979fb92490
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-06 15:00:00 +0200

    Add DAO, and remove useless files

---
+++ src/main/java/fr/univtln/wf/databases/daos/ExerciseDAO.java
@@ -0,0 +1,11 @@
+package fr.univtln.wf.databases.daos;
+
+import fr.univtln.wf.models.Exercise;
+
+/**
+ * DAO that manager Exercise class
+ * @author Wide Factory Team
+ */
+public class ExerciseDAO extends GenericDAO<Exercise>
+{
+}
---
+++ src/main/java/fr/univtln/wf/databases/daos/PersonDAO.java
@@ -0,0 +1,11 @@
+package fr.univtln.wf.databases.daos;
+
+import fr.univtln.wf.models.Person;
+
+/**
+ * DAO that manager Person class
+ * @author Wide Factory Team
+ */
+public class PersonDAO extends GenericDAO<Person>
+{
+}
