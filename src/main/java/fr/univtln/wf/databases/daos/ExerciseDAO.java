58e15afaf3caa1029ecf33e520665f98551fd206
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-09 15:59:00 +0200

    Remove query in ExerciseDAO

--- src/main/java/fr/univtln/wf/databases/daos/ExerciseDAO.java
+++ src/main/java/fr/univtln/wf/databases/daos/ExerciseDAO.java
@@ -1,16 +1,6 @@
 package fr.univtln.wf.databases.daos;
 
 import fr.univtln.wf.models.Exercise;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import jakarta.persistence.Query;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * DAO that manager Exercise class
@@ -18,17 +8,4 @@ import java.util.List;
  */
 public class ExerciseDAO extends GenericDAO<Exercise>
 {
-    EntityManagerFactory emf = Persistence
-            .createEntityManagerFactory("wf");
-    EntityManager em = emf.createEntityManager();
-
-
-
-
-    public List<String> getalexercises() {
-        Query query = entityManager.createQuery("Select e.name from Exercise e");
-        return query.getResultList();
-    }
-
-
 }
