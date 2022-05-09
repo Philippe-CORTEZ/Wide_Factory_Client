9a90578f069776a49ff5da476b27b1b4782529d7
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-09 14:36:56 +0200

    Refactor App class

--- src/main/java/fr/univtln/wf/App.java
+++ src/main/java/fr/univtln/wf/App.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -14,12 +16,27 @@ import java.util.Objects;
  */
 public class App extends Application
 {
+    /** Main stage */
+    @Getter
+    @Setter
+    private static Stage stage = new Stage();
+
+    /**
+     * Initialise a stage for JavaFX application
+     * @param primaryStage the stage initialized by JavaFX
+     * @throws IOException error raised when the file doesn't exist
+     */
     @Override
-    public void start(Stage stage) throws IOException
+    public void start(Stage primaryStage) throws IOException
     {
+        // Set the static stage with stage initialized by JavaFX
+        App.setStage(primaryStage);
+
+        // Load fxml file and display it
         Parent root = FXMLLoader.load(Objects.requireNonNull(App.class.getClassLoader().getResource("view/fxml/hello-view.fxml")));
         Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
+    
 }
