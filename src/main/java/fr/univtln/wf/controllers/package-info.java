e46f0ac57de8cc1bdacd21c0d463406b3ab34983
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-05-10 19:47:08 +0200

    Add package info in package controllers and daos

---
+++ src/main/java/fr/univtln/wf/controllers/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes that play the role of controllers of FXML views
+ * @author Wide Factory Team
+ */
+
+package fr.univtln.wf.controllers;
---
+++ src/main/java/fr/univtln/wf/databases/daos/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes that manage the database like DAOs
+ * @author Wide Factory Team
+ */
+
+package fr.univtln.wf.databases.daos;
